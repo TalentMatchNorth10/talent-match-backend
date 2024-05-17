2772839b8f68a6c351e4ff3dcf4d0ed23515b178
Jean Chien <jean.chien@caloudi.com>
2024-05-17 16:24:08 +0800

    refactor : studentReservation - validator

--- src/controllers/studentReservationController.ts
+++ src/controllers/studentReservationController.ts
@@ -1,3 +1,4 @@
+import { NextFunction, Request, Response } from 'express';
 import validator from 'validator';
 import appError from '../services/appError';
 import handleErrorAsync from '../services/handleErrorAsync';
@@ -8,11 +9,22 @@ const studentReservationController = {
   reserve_course: handleErrorAsync(
     async (req: Request, res: Response, next: NextFunction) => {
       const { course_id, teacher_id, student_id, reserve_time } = req.body;
+      
 
       if (!course_id || !teacher_id || !student_id || !reserve_time) {
         appError(400, '請填寫所有欄位', next);
       }
 
+      const existedReservation = await Reservation.find({
+        course_id: course_id,
+        teacher_id: teacher_id,
+        reserve_time: reserve_time
+      });
+      
+      console.log('existedReservation', existedReservation);
+      // if (existedReservation) {
+      //   appError(400, '該時段已被預約', next);
+      // }
       //   if (validator.isDate(reserve_time) === false) {
       //     appError(400, 'reserve_time 格式不正確', next);
       //   }
@@ -21,7 +33,7 @@ const studentReservationController = {
         course_id: course_id,
         teacher_id: teacher_id,
         student_id: student_id,
-        reserve_time: reserve_time,
+        reserve_time: new Date(reserve_time),
         teacher_status: 'reserved',
         student_status: 'reserved'
       });
