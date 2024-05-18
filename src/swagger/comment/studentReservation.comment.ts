c5485ddd3d0aa76b49767c99e3125fa907f74d37
hedgehog-chien <j830813@gmail.com>
2024-05-19 00:48:07 +0800

    feat: reservation_complete 功能

--- src/swagger/comment/studentReservation.comment.ts
+++ src/swagger/comment/studentReservation.comment.ts
@@ -23,6 +23,29 @@ const StudentReservationComment = {
           schema: { $ref: "#/components/schemas/Error500ResponseModel" }
       }
     */
+  },
+  reservation_complete: (req: Request, res: Response, next: NextFunction) => {
+    /**
+     * #swagger.tags = ['Student Reservation']
+     * #swagger.summary = '學生變更預約狀態為已完成'
+     * #swagger.requestBody = {
+            required: true,
+            content: {
+                "application/json": {
+                    schema: { $ref: "#/components/schemas/ReservationCompleteResponseModel" }
+                }
+            }
+        }
+      * #swagger.responses[200] = { 
+          schema: { $ref: "#/components/schemas/ReserveCourseResponseModel" }
+      }
+      * #swagger.responses[400] = { 
+          schema: { $ref: "#/components/schemas/Error400ResponseModel" }
+      }
+      * #swagger.responses[500] = { 
+          schema: { $ref: "#/components/schemas/Error500ResponseModel" }
+      }
+    */
   }
 };
 
