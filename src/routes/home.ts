50b452ca01533298afefcc528d5c04991a7eb5d1
hedgehog-chien <j830813@gmail.com>
2024-06-23 01:13:16 +0800

    feat: getCourseVideos - 取得首頁課程影片的api

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -3,12 +3,20 @@ import { isAuth } from '../services/auth';
 import teacherController from '../controllers/teacherController';
 const router = express.Router();
 import TeacherInfoComment from '../swagger/comment/teacherInfo.comment';
+import homeController from '../controllers/homeController';
+import HomeComment from '../swagger/comment/home.comment';
 
 router.post(
   '/teacher_apply',
   isAuth,
   TeacherInfoComment.postTeacherInfo,
   teacherController.postTeacherInfo
+);
+
+router.get(
+  '/course_videos/all',
+  HomeComment.getCourseVideos,
+  homeController.getCourseVideos
 );
 
 export default router;
