c00d6bb6343885684b561227bdd1025389388c3a
justine92415 <107650092+justine92415@users.noreply.github.com>
2024-06-23 21:15:16 +0800

    (fix) 修改已購買課程回傳欄位、新增課程評論API (#35)

--- src/swagger/schema/student.schema.ts
+++ src/swagger/schema/student.schema.ts
@@ -12,8 +12,10 @@ const StudentSchema = {
               type: 'object',
               properties: {
                 quantity_total: { type: 'number' },
+                review_count: { type: 'number' },
                 course_id: { type: 'string' },
                 course_name: { type: 'string' },
+                course_rate: { type: 'number' },
                 teacher_id: { type: 'string' },
                 teacher_name: { type: 'string' },
                 main_category: { type: 'string' },
@@ -25,9 +27,11 @@ const StudentSchema = {
                   items: {
                     type: 'object',
                     properties: {
+                      reserve_id: { type: 'string' },
                       reserve_time: { type: 'string' },
                       teacher_status: { type: 'string' },
-                      student_status: { type: 'string' }
+                      student_status: { type: 'string' },
+                      review: { type: 'string' }
                     }
                   }
                 }
@@ -43,8 +47,10 @@ const StudentSchema = {
         purchased_courses: [
           {
             quantity_total: 21,
+            review_count: 1,
             course_id: '66462077589368f14e1bf98e',
             course_name: '吉他基礎入門課程',
+            course_rate: 4.5,
             teacher_id: '66541517285a36fb1a197a0d',
             teacher_name: '柴可夫斯基',
             main_category: '音樂',
@@ -53,24 +59,32 @@ const StudentSchema = {
             remain_quantity: 17,
             reserved_course: [
               {
+                reserve_id: '666622f2589368f14e1d712a',
                 reserve_time: '2024-06-12T19:00:00.000Z',
                 teacher_status: 'reserved',
-                student_status: 'reserved'
+                student_status: 'reserved',
+                review: '666622f2589368f14e1d712a'
               },
               {
+                reserve_id: '666622f2589368f14e1d712a',
                 reserve_time: '2024-06-13T19:00:00.000Z',
                 teacher_status: 'reserved',
-                student_status: 'reserved'
+                student_status: 'reserved',
+                review: '666622f2589368f14e1d712a'
               },
               {
+                reserve_id: '666622f2589368f14e1d712a',
                 reserve_time: '2024-06-16T19:00:00.000Z',
                 teacher_status: 'reserved',
-                student_status: 'reserved'
+                student_status: 'reserved',
+                review: '666622f2589368f14e1d712a'
               },
               {
+                reserve_id: '666622f2589368f14e1d712a',
                 reserve_time: '2024-06-19T19:00:00.000Z',
                 teacher_status: 'reserved',
-                student_status: 'reserved'
+                student_status: 'reserved',
+                review: '666622f2589368f14e1d712a'
               }
             ]
           },
@@ -86,9 +100,11 @@ const StudentSchema = {
             remain_quantity: 29,
             reserved_course: [
               {
+                reserve_id: '666622f2589368f14e1d712a',
                 reserve_time: '2024-06-16T19:00:00.000Z',
                 teacher_status: 'reserved',
-                student_status: 'reserved'
+                student_status: 'reserved',
+                review: '666622f2589368f14e1d712a'
               }
             ]
           }
