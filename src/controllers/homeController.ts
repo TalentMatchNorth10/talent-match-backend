cfeeeea2714f4f956a852367a9c825f57082349c
Hedgehog Chien <97713208+hedgehog-chien@users.noreply.github.com>
2024-06-24 16:44:48 +0800

    feat: 首頁課程api (#38)
    - 取得課程api
    
    Co-authored-by: Jean Chien <jean.chien@caloudi.com>

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -3,21 +3,48 @@ import { NextFunction, Request, Response } from 'express';
 import handleErrorAsync from '../services/handleErrorAsync';
 import Video from '../models/videoModel';
 import appError from '../services/appError';
+import Course from '../models/courseModel';
 
-const homeController = {
+const HomeController = {
   getCourseVideos: handleErrorAsync(
     async (req: Request, res: Response, next: NextFunction) => {
       const { main_category } = req.query;
 
+      const query: any = {};
+
+      if (main_category) {
+        query.category = main_category;
+      }
+
       try {
         let videos = [];
-        if (main_category) {
-          videos = await Video.find({ category: main_category }).limit(10);
-          handleSuccess(res, videos);
-        } else {
-          videos = await Video.find().limit(10);
-          handleSuccess(res, videos);
-        }
+        console.log(query);
+        videos = await Video.find(query).limit(10);
+        handleSuccess(res, videos);
+      } catch (err) {
+        return appError(500, `伺服器錯誤`, next);
+      }
+    }
+  ),
+
+  getCourses: handleErrorAsync(
+    async (req: Request, res: Response, next: NextFunction) => {
+      const { main_category, city_id } = req.query;
+
+      const query: any = {};
+
+      if (main_category) {
+        query.main_category = main_category;
+      }
+
+      if (city_id) {
+        query.city_id = city_id;
+      }
+
+      try {
+        let courses = [];
+        courses = await Course.find(query).limit(10);
+        handleSuccess(res, courses);
       } catch (err) {
         return appError(500, `伺服器錯誤`, next);
       }
@@ -25,4 +52,4 @@ const homeController = {
   )
 };
 
-export default homeController;
+export default HomeController;
